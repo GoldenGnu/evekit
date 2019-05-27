29a0b044659b612542f20303ca076d09aa31a112
GoldenGnu <GoldenGnu@users.noreply.github.com>
2019-05-27 11:11:25 +0200

    Code: Moved to openapi-generator 4.0.0

--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
 rmdir /s /q %~dp0src
 rmdir /s /q %~dp0gradle
-java -jar openapi-generator-cli.jar generate -i https://evekit-model.orbital.enterprises/api/swagger.json -g java -c config.json
+java -jar openapi-generator-cli-4.0.0.jar generate -i https://evekit-model.orbital.enterprises/api/swagger.json -g java -c config.json
 pause
